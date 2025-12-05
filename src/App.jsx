1417da8c9f99502bf08b98d6b460bc879bf45918
JT <jstorbit@gmail.com>
2025-12-04 17:46:21 -0700

    Create and add Product components to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import products from './data'
+import Product from './Product'
 
 function App() {
 
   return (
     <>
       <h1>Hello World</h1>
-      
+      <Product product={products[0]} />
+      <Product product={products[1]} />
+      <Product product={products[2]} />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Product.jsx
@@ -0,0 +1,10 @@
+const Product = (product) => {
+  return (
+    <section>
+      <h2>Product</h2>
+      <p>{product}</p>
+    </section>
+  );
+}
+
+export default Product;
